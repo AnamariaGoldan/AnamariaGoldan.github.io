93ad7d15fdb07976700c8c995133cd6cc8f351c5
AnamariaGoldan <48368498+AnamariaGoldan@users.noreply.github.com>
2020-05-24 20:23:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $connection = mysqli_connect($host,$user,$pass,$db);
 $query = "select * from meniu_filtrare";
 $result = mysqli_query($connection, $query);
 while ($row = $result->fetch_assoc()){
-    echo $row['nume_filtru'] . ' ' .  $row['subcategorii'];
+    echo $row['nume_filtru'] . ' ' .  $row['subcategorii'] . 'sdfasdf';
 }
 
 ?>
