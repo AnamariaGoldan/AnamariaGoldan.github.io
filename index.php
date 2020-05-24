0190d20bf1a2eabb3aca546cf5750eb37065bff4
AnamariaGoldan <48368498+AnamariaGoldan@users.noreply.github.com>
2020-05-24 20:25:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-
+echo "aasdf";
 $host = 'sql7.freemysqlhosting.net';
 $user = 'sql7343022';
 $pass = 'NZKVkwW18G';
 $db = 'sql7343022';
+echo "aasdf";
 $connection = mysqli_connect($host,$user,$pass,$db);
+echo "aasdf";
 $query = "select * from meniu_filtrare";
 $result = mysqli_query($connection, $query);
 while ($row = $result->fetch_assoc()){
